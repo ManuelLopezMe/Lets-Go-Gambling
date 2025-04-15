531f6281f4d91417ea1f0b16ebe5a21019d8fc88
ManuelLopezMe <53957443+ManuelLopezMe@users.noreply.github.com>
2025-04-14 20:36:52 -0400

    Create player_choices.py
    Basic choices whether to hit or stand

---
+++ player_choices.py
@@ -0,0 +1,12 @@
+class choices:
+    def __init__(self, player_hand: list, deck = list):
+        self.player_hand = player_hand
+        self.deck = deck
+
+    def hit(self):
+        self.player_hand.append(deal_card(self.deck))
+    
+    def stand(self):
+        return True
+    
+  
