950e430ed0220f467e075958eb8011b70ac35d1d
ManuelLopezMe <53957443+ManuelLopezMe@users.noreply.github.com>
2025-04-16 14:14:38 -0400

    add annotations to game_engine.py

--- Game_Engine.py
+++ Game_Engine.py
@@ -3,15 +3,11 @@ from player_choices import *
 from metrics import *
 from deck import *
 
-# create initial deck
-deck = Deck(num_decks=2)
-deck.shuffle()
-
 class PlayGame:
     def __init__(self):
         pass
     
-    def play_round(self, wager, player_hand, dealer_hand):
+    def play_round(self, wager, player_hand, dealer_hand, deck):
             value = Hand(player_hand)
             player_value = value.compute_value()
             
@@ -26,22 +22,22 @@ class PlayGame:
                 action = input('Hit, stand, double, or split?').lower()
 
                 if action == 'hit':
-                    hand = PlayHand(player_hand, None, self.deck)
+                    hand = PlayHand(player_hand, None, deck)
                     player_value=hand.player_turn(action)
                     if player_value > 21:
                         print("You Busted")
                         return 0 - wager
                 if action == 'stand':
-                    hand = PlayHand(player_hand, None, self.deck)
+                    hand = PlayHand(player_hand, None, deck)
                     player_value=hand.player_turn(action)
                 
                 if action == 'double':
-                    hand=PlayHand(player_hand, None, self.deck)
+                    hand=PlayHand(player_hand, None, deck)
                     player_value=hand.player_turn(action)
                     if player_value > 21:
                         print('You busted!')
                         return 0 - (2*wager)
-                    else:
+                    else: ##double down only allows one hit
                         while dealer_value < 21 or dealer_value <= player_value:
                             dealer_value=hand.dealer_turn()
                         if player_value > dealer_value:
@@ -65,35 +61,37 @@ class PlayGame:
                     while right_value <= 21: 
                         right_action = input('Hit, stand, or double?').lower()
                         if right_action == 'double':
-                            right = PlayHand(player_hand=right_hand, None, self.deck)
+                            right = PlayHand(player_hand=right_hand, None, deck)
                             right_value = right.player_turn(action)
                             break
                         if right_action == 'hit':
-                            right = PlayHand(player_hand=right_hand, None, self.deck)
+                            right = PlayHand(player_hand=right_hand, None, deck)
                             right_value=right.player_turn(action)
                         if right_action == 'stand':
-                            right = PlayHand(player_hand=right_hand, None, self.deck)
+                            right = PlayHand(player_hand=right_hand, None, deck)
                             right_value=right.player_turn(action)
                             break
                     
-
+                    # play left hand
                     while left_value <= 21:
                         left_action = input('Hit, stand, or double?').lower()
                         if left_action == 'double':
-                            left = PlayHand(left_hand=right_hand, None, self.deck)
+                            left = PlayHand(left_hand=right_hand, None, deck)
                             left_value = right.player_turn(action)
                             break
                         if left_action == 'hit':
-                            left = PlayHand(left_hand=right_hand, None, self.deck)
+                            left = PlayHand(left_hand=right_hand, None, deck)
                             left_value=left.player_turn(action)
                         if left_action == 'stand':
-                            left = PlayHand(left_hand=right_hand, None, self.deck)
+                            left = PlayHand(left_hand=right_hand, None, deck)
                             left_value=left.player_turn(action)
                             break
                     while dealer_value < 21:
-                        x, dealer_value=hand.dealer_turn()
+                        x, dealer_value=PlayHand(None, dealer_hand, deck).dealer_turn()
                         if x == 'done':
                             break
+
+                    ## Possible Payouts
                     if left_value > dealer_value and right_value > dealer_value:
                         if left_action == 'double' and right_action == 'double':
                             return 4*wager
@@ -131,7 +129,7 @@ class PlayGame:
                             return 0
                     
                 while dealer_value < 21:
-                    x, dealer_value=hand.dealer_turn()
+                    x, dealer_value=PlayHand(None, dealer_hand, deck).dealer_turn()
                     if x == 'done':
                         break
                 if player_value > dealer_value:
@@ -148,20 +146,22 @@ class PlayGame:
                         return 0 - (2*wager)
     
     def play_game(self, bankroll=100, num_rounds=10, num_decks = 2):
-        rounds_played = 0
+        rounds_played = 0 #count how many rounds have been played
+        
+        # create starting deck
         deck = Deck(num_decks)
         deck.shuffle()
         total_deck_size = len(deck)
-        while (rounds_played <= num_rounds) or bankroll > 0:
+        while (rounds_played <= num_rounds) or bankroll > 0: #play until we've reach round limit or if player goes bankrupt
             wager = int(input('select wager amount'))
             player_hand = deck.deal_hand()
             dealer_hand = deck.deal_hand()
-            if wager > self.bankroll:
+            if wager > self.bankroll: #wager can't be higher than the players bankroll
                 print("Not enough to wager, enter amount less than ${bankroll}".format(bankroll))
                 wager = int(input('select wager amount'))
-            bankroll += self.play_round(wager, player_hand, dealer_hand)
+            bankroll += self.play_round(wager, player_hand, dealer_hand, deck)
             rounds_played += 1
-            if len(deck) <= total_deck_size*.2:
+            if len(deck) <= round(total_deck_size*.2): #reshuffle after we've gone through 20% of the deck
                 deck.shuffle()
 
 class PlayHand:
@@ -175,21 +175,13 @@ class PlayHand:
         actions = Choices(self.player_hand)
         value = Hand(self.player_hand)
         
-        if action == 'hit':
+        if action == 'hit' or action == 'double':
             actions.hit()
             player_value = value.compute_value()
             return player_value
         elif action == 'stand':
             player_value = value.compute_value()
             return player_value
-        elif action == 'double':
-            actions.hit()
-            player_value = value.compute_value()
-            if player_value > 21:
-                print("You busted!")
-                return player_value
-            else:
-                return player_value
        
     def dealer_turn(self):
         actions = Choices(self.dealer_hand)
@@ -202,9 +194,8 @@ class PlayHand:
             actions.hit()
             dealer_value
             if dealer_value > 21:
-                "Dealer busted!"
                 return 'done', dealer_value
 
-PlayGame()
+PlayGame().play_game()
                     
 print("Thanks for playing!")
