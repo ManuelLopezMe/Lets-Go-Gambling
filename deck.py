540d5754671d81b988d2e430bc39e519c0eb55dc
ManuelLopezMe <53957443+ManuelLopezMe@users.noreply.github.com>
2025-04-16 14:25:13 -0400

    Update deck.py
    added return to shuffle method and EOL

--- deck.py
+++ deck.py
@@ -1,6 +1,7 @@
 import numpy as np
 from player_choices import *
-suits = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] * 4
+
+suits = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 class Deck:
     def __init__(self, num_decks):
@@ -9,9 +10,11 @@ class Deck:
 
     def shuffle(self):
         np.random.shuffle(self.deck)
+        return self.deck
     
     def deal_card(self):
         return self.deck.pop()
     
     def deal_hand(self):
         return [self.deck.pop(), self.deck.pop()]
+
